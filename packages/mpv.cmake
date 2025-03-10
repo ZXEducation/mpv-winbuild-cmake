7b6e74b2a80ea7f269acad3fafbc2cb46931e868
ZXEducation <developer@zhixing.co>
2025-03-10 14:41:22 +0800

    测试

--- packages/mpv.cmake
+++ packages/mpv.cmake
@@ -23,7 +23,7 @@ ExternalProject_Add(mpv
         vapoursynth
         libsdl2
     GIT_REPOSITORY https://github.com/ZXEducation/mpv.git
-    GIT_TAG cartrack
+    GIT_TAG dev
     SOURCE_DIR ${SOURCE_LOCATION}
     GIT_CLONE_FLAGS "--filter=tree:0"
     UPDATE_COMMAND ""
